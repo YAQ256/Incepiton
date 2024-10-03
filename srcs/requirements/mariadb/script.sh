58ab0c2f0aa06344464977217ad8ae71cbd3e10d
cyacoub- <cyacoub-@Inception.myguest.virtualbox.org>
2024-10-03 14:37:37 +0200

    terminado, falta organizar archivos

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" > /etc/mysql/init.sql
+echo "CREATE USER IF NOT EXISTS '$dbuser'@'%' IDENTIFIED BY '$dbpassword';" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$dbuser'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$root_pass' ;" >> /etc/mysql/init.sql
+echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+
 mysql_install_db
 mysqld
